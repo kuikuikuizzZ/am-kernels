d5e8daef092b8ea41cf31f365fbd3690f6d8faa4
kuikuikuizzZ <kimmmywu@gmail.com>
2025-09-30 11:26:07 +0800

    feat(test): support sim-iverilog

--- tests/am-tests/src/main.c
+++ tests/am-tests/src/main.c
@@ -24,7 +24,7 @@ int main(const char *args) {
     CASE('t', rtc_test, IOE);
     CASE('k', keyboard_test, IOE);
     CASE('v', video_test, IOE);
-    CASE('a', audio_test, IOE);
+    // CASE('a', audio_test, IOE);
     CASE('p', vm_test, CTE(vm_handler), VME(simple_pgalloc, simple_pgfree));
     case 'H':
     default:
